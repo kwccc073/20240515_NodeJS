caf26b96de69811ebe340921eb3c521e9e630db9
kaiwen <kaiwen073c@gmail.com>
2024-06-17 09:37:39 +0800

    修正錯誤

--- commands/twgod.js
+++ commands/twgod.js
@@ -1,29 +1,29 @@
 // 引入套件
-import axios from "axios";
+import axios from 'axios'
 // 計算距離的工具檔案
-import { distance } from "../utils/distance.js";
+import { distance } from '../utils/distance.js'
 // LINE訊息的模板
-import template from "../templates/taiwangods.js";
+import template from '../templates/taiwangods.js'
 // node: 表示node js內建的套件
 // fs表示檔案處理，可以讀也可以寫
 // 用於訊息模板掛掉時，用來複製他
-import fs from "node:fs";
+import fs from 'node:fs'
 
 export default async (event) => {
   try {
     // URLSearchParams()讓你可以方便地解析、操作和構建URL的查詢字符串。（特定資料型態才會用到）
     const { data } = await axios.post(
-      "https://taiwangods.moi.gov.tw/Control/SearchDataViewer.ashx?t=landscape",
+      'https://taiwangods.moi.gov.tw/Control/SearchDataViewer.ashx?t=landscape',
       new URLSearchParams({
         lang: 1,
-        area: "",
-        rtype: "",
-        festival: "",
-        keyWord: "",
-        festival_s: "",
-        festival_e: "",
+        area: '',
+        rtype: '',
+        festival: '',
+        keyWord: '',
+        festival_s: '',
+        festival_e: ''
       })
-    );
+    )
     const replies = data
       .map((d) => {
         // 對陣列每個東西(d)裡面加上distance欄位，distance為一個陣列，其內容如下方括弧
@@ -35,55 +35,55 @@ export default async (event) => {
           event.message.latitude,
           event.message.longitude,
           // 使用者離景點距離（公里）
-          "K"
-        );
-        return d;
+          'K'
+        )
+        return d
       })
       //   陣列排序
       .sort((a, b) => {
         // a-b是正序排，由近到遠
-        return a.distance - b.distance;
+        return a.distance - b.distance
       })
       //   .slice(開始位置,結束位置)，(0,5)即取五個資料
       .slice(0, 5)
       .map((d) => {
         // 變成訊息模板的型態
         // 註：template在../templates/taiwangods.js裡是函式
-        const t = template(); //訊息模板
+        const t = template() // 訊息模板
         // 地址名稱
-        t.body.contents[0].text = d.LL_Title;
-        t.body.contents[1].text = d.LL_Highlights;
+        t.body.contents[0].text = d.LL_Title
+        t.body.contents[1].text = d.LL_Highlights
         t.body.contents[2].contents[0].contents[1].text =
-          d.LL_Country + d.LL_Area + d.LL_Address;
-        t.body.contents[2].contents[1].contents[1].text = d.LL_OpeningData;
-        t.body.contents[2].contents[2].contents[1].text = d.LL_OpeningTime;
+          d.LL_Country + d.LL_Area + d.LL_Address
+        t.body.contents[2].contents[1].contents[1].text = d.LL_OpeningData
+        t.body.contents[2].contents[2].contents[1].text = d.LL_OpeningTime
         // 地圖網址（d.L_MapY、d.L_MapX為經緯度）
-        t.footer.contents[0].action.uri = `https://www.google.com/maps/search/?api=1&query=${d.L_MapY},${d.L_MapX}`;
-        t.footer.contents[1].action.uri = `https://taiwangods.moi.gov.tw/html/landscape/1_0011.aspx?i=${d.L_ID}`;
-        return t;
-      });
+        t.footer.contents[0].action.uri = `https://www.google.com/maps/search/?api=1&query=${d.L_MapY},${d.L_MapX}`
+        t.footer.contents[1].action.uri = `https://taiwangods.moi.gov.tw/html/landscape/1_0011.aspx?i=${d.L_ID}`
+        return t
+      })
     // 傳送訊息
     const result = await event.reply({
-      type: "flex",
-      //替代文字，LINE沒有進入聊天室會看到的訊息
-      altText: "宗教文化查詢結果",
+      type: 'flex',
+      // 替代文字，LINE沒有進入聊天室會看到的訊息
+      altText: '宗教文化查詢結果',
       // 多張卡片的寫法
       contents: {
-        type: "carousel",
-        contents: replies,
-      },
-    });
-    if (process.env.DEBUG === "true") {
-      console.log(result);
+        type: 'carousel',
+        contents: replies
+      }
+    })
+    if (process.env.DEBUG === 'true') {
+      console.log(result)
       // 這邊用於debug
       if (result.message) {
         // .writeFileSync(寫入的檔案,寫入的內容)把內容寫在指定的檔案裡面
         // .stringify()將 JavaScript 對象或值轉換為 JSON 字符串的方法。(內容,把裡面的東西做取代,幾個縮排)，用於排版
-        fs.writeFileSync("./dump/twgod.json", JSON.stringify(replies, null, 2));
+        fs.writeFileSync('./dump/twgod.json', JSON.stringify(replies, null, 2))
       }
     }
   } catch (error) {
-    console.log(error);
-    event.reply("發生錯誤");
+    console.log(error)
+    event.reply('發生錯誤')
   }
-};
+}
--- index.js
+++ index.js
@@ -1,97 +1,97 @@
 // 1. 引入套件
-import "dotenv/config"; // 用於讀取環境設定檔(.env)
-import linebot from "linebot"; //引用機器人
+import 'dotenv/config' // 用於讀取環境設定檔(.env)
+import linebot from 'linebot' // 引用機器人
 // 5.引入commands裡設定的指令
-import commandFE from "./commands/fe.js";
-import commandTWGod from "./commands/twgod.js";
-import commandUsd from "./commands/usd.js";
-import { scheduleJob } from "node-schedule";
-import * as usdtwd from "./data/usdtwd.js";
+import commandFE from './commands/fe.js'
+import commandTWGod from './commands/twgod.js'
+import commandUsd from './commands/usd.js'
+import { scheduleJob } from 'node-schedule'
+import * as usdtwd from './data/usdtwd.js'
 
 // https://crontab.guru/#0_5_*_*_*
 // 設定排程，每天早上五點就會執行這個function來更新匯率，可以節省找資料的時間
-scheduleJob("0 5 * * *", () => {
-  usdtwd.update();
-});
+scheduleJob('0 5 * * *', () => {
+  usdtwd.update()
+})
 // 使啟動機器人的時候也會進行這個function
-usdtwd.update();
+usdtwd.update()
 
 // 2. 設定機器人
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-});
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
 
 // 4. 當機器人收到訊息(message)時要執行的程式碼
 // event表示訊息事件的資訊， 包含了訊息的類型、文字等
-bot.on("message", (event) => {
+bot.on('message', (event) => {
   // process.env.DEBUG的true為文字，要用''框起來
   // DEBUG用於判斷本機開發還是雲端伺服器，上傳雲端的時候可以將.env裡的DEBUG設定為false，這樣就不會有任何作用
-  if (process.env.DEBUG === "true") {
-    console.log(event);
+  if (process.env.DEBUG === 'true') {
+    console.log(event)
   }
-  if (event.message.type === "text") {
+  if (event.message.type === 'text') {
     // event.message.text為使用者傳送的文字
-    if (event.message.text === "前端") {
+    if (event.message.text === '前端') {
       // commandFE()待確認
-      commandFE(event);
-    } else if (event.message.text === "usd") {
+      commandFE(event)
+    } else if (event.message.text === 'usd') {
       // commandUsd()待確認
-      commandUsd(event);
-    } else if (event.message.text === "qr") {
+      commandUsd(event)
+    } else if (event.message.text === 'qr') {
       // 這裡是quick reply
       // event.reply 為機器人回覆的訊息
       event.reply({
-        type: "text",
-        text: "123",
+        type: 'text',
+        text: '123',
         quickReply: {
           items: [
             {
-              type: "action",
+              type: 'action',
               action: {
-                type: "message",
+                type: 'message',
                 // 按下去使用者會傳送出的文字
-                text: "ubike:taipei",
+                text: 'ubike:taipei',
                 // 按鈕文字
-                label: "taipei",
-              },
+                label: 'taipei'
+              }
             },
             {
-              type: "action",
+              type: 'action',
               action: {
-                type: "uri",
-                uri: "https://wdaweb.github.io",
-                label: "職訓",
-              },
+                type: 'uri',
+                uri: 'https://wdaweb.github.io',
+                label: '職訓'
+              }
             },
             {
-              type: "action",
+              type: 'action',
               action: {
                 // 使使用者傳送的訊息不會出現在聊天室
-                type: "postback",
-                uri: "https://wdaweb.github.io",
-                label: "職訓",
-                data: "aaa",
-              },
-            },
-          ],
-        },
-      });
+                type: 'postback',
+                uri: 'https://wdaweb.github.io',
+                label: '職訓',
+                data: 'aaa'
+              }
+            }
+          ]
+        }
+      })
     }
-  } else if (event.message.type === "location") {
+  } else if (event.message.type === 'location') {
     // commandTWGod待確認
-    commandTWGod(event);
+    commandTWGod(event)
   }
-});
+})
 
-bot.on("postback", (event) => {
-  console.log(event);
-  event.reply("aaa");
-});
+bot.on('postback', (event) => {
+  console.log(event)
+  event.reply('aaa')
+})
 
 // 3. 設定機器人的網頁伺服器，去監聽指的 port跟指定的路徑的請求
 // process.env.PORT || 3000 表示：若環境變數裡有PORT就用它，沒有就用3000
-bot.listen("/", process.env.PORT || 3000, () => {
-  console.log("機器人啟動");
-});
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('機器人啟動')
+})
